ca96c3a56f717f586f91b65224549dbdb21fa791
CybersRMUTL <cybers@rmutl.ac.th>
2026-05-25 12:29:25 +0700

    Update findflag.php

--- findflag.php
+++ findflag.php
@@ -1,32 +1,28 @@
 <?php
 
-$dir = "/root/vuln-labs/rfi/secret/";
+$dir = "/var/www/html/rfi/secret/";
 
 if (is_dir($dir)) {
 
     $files = scandir($dir);
 
-    echo "<h2>Files:</h2>";
-
     foreach ($files as $file) {
 
         if ($file != "." && $file != "..") {
 
-            echo $file . "<br>";
-
             $path = $dir . $file;
 
-            if (is_file($path)) {
+            echo "<h3>$file</h3>";
 
-                echo "<pre>";
-                echo htmlspecialchars(file_get_contents($path));
-                echo "</pre>";
-            }
+            echo "<pre>";
+            echo htmlspecialchars(file_get_contents($path));
+            echo "</pre>";
         }
     }
 
 } else {
 
     echo "Directory not found";
+
 }
 ?>
